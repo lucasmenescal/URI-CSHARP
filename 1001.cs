4c9fbb06755dd1c012dbed02629fea9e5c8dee9a
lucasmenescal <menes_pontes@hotmail.com>
2020-09-07 16:48:04 -0300

    Update 1

---
+++ 1001.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int A, B, X;
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        X = A + B;
+        System.Console.WriteLine("X = "+X);
+        }
+    }
+}
---
+++ 1002.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+//saida de dados Console.WriteLine()
+//entrada de dados Console.ReadLine()
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double area, n, raio;
+
+        n = 3.14159;
+        raio = double.Parse(Console.ReadLine(),CI);
+        area = n * (raio*raio);
+        System.Console.WriteLine("A="+area.ToString("F4", CI));
+        }
+    }
+}
